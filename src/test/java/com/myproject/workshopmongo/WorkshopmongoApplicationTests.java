ffa383d7dc1cc847d93181d4b2200f1b63e61700
Victor Almeida <victorh.almeida7@gmail.com>
2022-01-28 20:36:00 -0300

    Criação do projeto

---
+++ src/test/java/com/myproject/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
